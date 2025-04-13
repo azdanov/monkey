bea7a36af75f8760b2d99b2feeefcfbc76098e67
Anton Ždanov <anton@azdanov.dev>
2025-04-13 10:16:00 +0300

    add repl

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
-package monkey
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/azdanov/monkey/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(os.Stdout, "Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Fprintln(os.Stdout, "Feel free to type in commands")
+	repl.Start(os.Stdin, os.Stdout)
+}
